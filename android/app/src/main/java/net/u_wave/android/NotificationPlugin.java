8277a2a71af6332ae43ded24306c6aaff40b738d
Renée Kooi <renee@kooi.me>
2018-10-07 00:12:35 +0200

    fix

--- android/app/src/main/java/net/u_wave/android/NotificationPlugin.java
+++ android/app/src/main/java/net/u_wave/android/NotificationPlugin.java
@@ -1,8 +1,9 @@
 package net.u_wave.android;
 
 import java.util.Map;
-import android.app.Notification;
-import android.app.NotificationManager;
+import android.R;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -20,23 +21,28 @@ public class NotificationPlugin implements MethodCallHandler {
   }
 
   private final Registrar registrar;
+  private NotificationCompat.Builder notificationBuilder;
 
   private NotificationPlugin(Registrar registrar) {
     this.registrar = registrar;
+
+    notificationBuilder = new NotificationCompat.Builder(registrar.context(), NAME)
+      .setSmallIcon(R.drawable.ic_media_play)
+      .setOngoing(true);
   }
 
   private void onNowPlaying(Map<String, String> args, Result result) {
-    NotificationManager manager = registrar.context()
-      .getSystemService(NotificationManager.class);
-    Notification.Style style = new Notification.MediaStyle();
-    manager.notify(NOTIFY_NOW_PLAYING,
-        new Notification.Builder(registrar.context())
-          .setContentTitle(args.get("title"))
-          .setContentText(args.get("artist"))
-          .setStyle(style)
-          .setOngoing(true)
-          .setProgress(100, 70, false)
-          .build());
+    System.out.println("[NotificationPlugin] nowPlaying: " + args.get("artist") + " - " + args.get("title"));
+
+    notificationBuilder
+      .setContentTitle(args.get("title"))
+      .setContentText(args.get("artist"))
+      .setProgress(100, 70, false)
+      .build();
+
+    NotificationManagerCompat manager = NotificationManagerCompat.from(registrar.context());
+    manager.notify(NOTIFY_NOW_PLAYING, notificationBuilder.build());
+
     result.success(null);
   }
 
