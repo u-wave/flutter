b71f1ac19df5b564a3d1e2ef2d614320e3496b8e
Renée Kooi <renee@kooi.me>
2018-10-15 09:52:37 +0200

    More markup tests

--- test/markup_test.dart
+++ test/markup_test.dart
@@ -8,7 +8,86 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:uw_flutter/u_wave/markup.dart';
 
 void main() {
-  test('Markup', () async {
+  test('bold', () {
+    expect(MarkupParser(source: 'some *bold* text').parse().join(', '), equals(<MarkupNode>[
+      TextNode(text: 'some '),
+      BoldNode(content: <MarkupNode>[
+        TextNode(text: 'bold'),
+      ]),
+      TextNode(text: ' text'),
+    ].join(', ')));
+  });
+
+  test('italic', () {
+    expect(MarkupParser(source: 'some _italic_ text').parse().join(', '), equals(<MarkupNode>[
+      TextNode(text: 'some '),
+      ItalicNode(content: <MarkupNode>[
+        TextNode(text: 'italic'),
+      ]),
+      TextNode(text: ' text'),
+    ].join(', ')));
+  });
+
+  test('strike', () {
+    expect(MarkupParser(source: 'some ~stroke~ text').parse().join(', '), equals(<MarkupNode>[
+      TextNode(text: 'some '),
+      StrikeNode(content: <MarkupNode>[
+        TextNode(text: 'stroke'),
+      ]),
+      TextNode(text: ' text'),
+    ].join(', ')));
+  });
+
+  test('code', () {
+    expect(MarkupParser(source: 'some `monospace` text').parse().join(', '), equals(<MarkupNode>[
+      TextNode(text: 'some '),
+      CodeNode(text: 'monospace'),
+      TextNode(text: ' text'),
+    ].join(', ')));
+  });
+
+  test('markup characters in the middle of a word', () {
+    expect(MarkupParser(source: 'underscored_words are fun_!').parse().join(', '), equals(<MarkupNode>[
+      TextNode(text: 'underscored_words are fun_!'),
+    ].join(', ')));
+  });
+
+  test('stray markup characters', () {
+    expect(MarkupParser(source: 'a * b').parse().join(', '), equals(<MarkupNode>[
+      TextNode(text: 'a * b'),
+    ].join(', ')));
+  });
+
+  test('nested',  () {
+    expect(MarkupParser(source: '*bold _italic_*').parse().join(', '), equals(<MarkupNode>[
+      BoldNode(content: <MarkupNode>[
+        TextNode(text: 'bold '),
+        ItalicNode(content: <MarkupNode>[
+          TextNode(text: 'italic'),
+        ]),
+      ]),
+    ].join(', ')));
+  });
+
+  test('code nested inside markup', () {
+    expect(MarkupParser(source: '*_`monospace`_*').parse().join(', '), equals(<MarkupNode>[
+      BoldNode(content: <MarkupNode>[
+        ItalicNode(content: <MarkupNode>[
+          CodeNode(text: 'monospace'),
+        ]),
+      ]),
+    ].join(', ')));
+  });
+
+  test('markup nested inside code', () {
+    expect(MarkupParser(source: 'a `b *c* _d_` e').parse().join(', '), equals(<MarkupNode>[
+      TextNode(text: 'a '),
+      CodeNode(text: 'b *c* _d_'),
+      TextNode(text: ' e'),
+    ].join(', ')));
+  });
+
+  test('Markup', () {
     List<MarkupNode> parts = MarkupParser(source: '*bold* _italic_').parse();
     expect(parts[0], isInstanceOf<BoldNode>());
     expect(parts[1], isInstanceOf<TextNode>());
