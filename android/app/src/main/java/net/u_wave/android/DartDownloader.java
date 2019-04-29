a6a3ba4177c90aeb64f133b1205bc83e4e258a7f
Renée Kooi <renee@kooi.me>
2019-04-29 12:59:14 +0200

    Update NewPipe, pin dependencies to pre-AndroidX

--- android/app/src/main/java/net/u_wave/android/DartDownloader.java
+++ android/app/src/main/java/net/u_wave/android/DartDownloader.java
@@ -3,9 +3,12 @@ package net.u_wave.android;
 import android.util.Log;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.Result;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import org.schabi.newpipe.extractor.Downloader;
+import org.schabi.newpipe.extractor.DownloadRequest;
+import org.schabi.newpipe.extractor.DownloadResponse;
 import org.schabi.newpipe.extractor.utils.Localization;
 
 /**
@@ -58,6 +61,21 @@ class DartDownloader implements Downloader {
     }
 
     return result.getResponse();
+  }
+
+  @Override
+  public DownloadResponse get(String url, DownloadRequest r) throws IOException {
+    throw new IOException("unimplemented");
+  }
+
+  @Override
+  public DownloadResponse get(String url) throws IOException {
+    throw new IOException("unimplemented");
+  }
+
+  @Override
+  public DownloadResponse post(String url, DownloadRequest r) throws IOException {
+    throw new IOException("unimplemented");
   }
 
   class DownloadResult implements Result {
